e94ca359d63f8b7741fccea1a71c1007127b8801
George Melikov <mail@gmelikov.ru>
2019-09-10 23:34:53 +0300

    zio_compress: use spa_max_ashift as threshold
    Now default compression is lz4, which can stop
    compression process by itself on incompressible data.
    If there is additional size checks -
    we will only make our compressratio worse.
    So we can check only for sector size.
    
    If data wasn't compressed - it will be saved as
    ZIO_COMPRESS_OFF, so if we really need to recompress
    data without ashift info and check anything -
    we can just compress it with zero threshold.
    So, we don't need a new feature flag here!
    
    Signed-off-by: George Melikov <mail@gmelikov.ru>

--- module/zfs/arc.c
+++ module/zfs/arc.c
@@ -26,6 +26,7 @@
  * Copyright (c) 2017, Nexenta Systems, Inc.  All rights reserved.
  * Copyright (c) 2019, loli10K <ezomori.nozomu@gmail.com>. All rights reserved.
  * Copyright (c) 2020, George Amanakis. All rights reserved.
+ * Copyright (c) 2020 by George Melikov. All rights reserved.
  */
 
 /*
@@ -1755,7 +1756,7 @@ arc_hdr_authenticate(arc_buf_hdr_t *hdr, spa_t *spa, uint64_t dsobj)
 		abd_take_ownership_of_buf(abd, B_TRUE);
 
 		csize = zio_compress_data(HDR_GET_COMPRESS(hdr),
-		    hdr->b_l1hdr.b_pabd, tmpbuf, lsize);
+		    hdr->b_l1hdr.b_pabd, tmpbuf, lsize, 0);
 		ASSERT3U(csize, <=, psize);
 		abd_zero_off(abd, csize, psize - csize);
 	}
@@ -8531,7 +8532,8 @@ l2arc_apply_transforms(spa_t *spa, arc_buf_hdr_t *hdr, uint64_t asize,
 		cabd = abd_alloc_for_io(asize, ismd);
 		tmp = abd_borrow_buf(cabd, asize);
 
-		psize = zio_compress_data(compress, to_write, tmp, size);
+		psize = zio_compress_data(compress, to_write, tmp, size,
+		    spa->spa_max_ashift);
 		ASSERT3U(psize, <=, HDR_GET_PSIZE(hdr));
 		if (psize < asize)
 			bzero((char *)tmp + psize, asize - psize);
@@ -9899,7 +9901,7 @@ l2arc_log_blk_commit(l2arc_dev_t *dev, zio_t *pio, l2arc_write_callback_t *cb)
 	/* try to compress the buffer */
 	list_insert_tail(&cb->l2wcb_abd_list, abd_buf);
 	psize = zio_compress_data(ZIO_COMPRESS_LZ4,
-	    abd_buf->abd, tmpbuf, sizeof (*lb));
+	    abd_buf->abd, tmpbuf, sizeof (*lb), dev->l2ad_spa->spa_max_ashift);
 
 	/* a log block is never entirely zero */
 	ASSERT(psize != 0);
